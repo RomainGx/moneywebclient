9eb41294d0b36dad6bbdaf4823a555d7c21cfac5
Romain Guidoux <rguidoux.dev@gmail.com>
2015-08-05 22:51:02 +0200

    Base du projet

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name moneyWebClientApp
+ * @description
+ * # moneyWebClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('moneyWebClientApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moneyWebClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moneyWebClientApp
+ */
+angular.module('moneyWebClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
