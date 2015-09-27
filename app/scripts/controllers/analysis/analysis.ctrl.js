f100fb17eb94bbd207d2ea72c8a4f6b7017c5bb3
Romain Guidoux <rguidoux.dev@gmail.com>
2015-09-27 14:14:55 +0200

    Création d'un contrôleur pour la page d'accueil des analyses

---
+++ app/scripts/controllers/analysis/analysis.ctrl.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('moneyWebClientApp')
+    .controller('AnalysisCtrl', AnalysisCtrl);
+
+
+  AnalysisCtrl.$inject = [];
+  function AnalysisCtrl()
+  {
+
+  }
+})();
